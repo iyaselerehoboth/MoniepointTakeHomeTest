4819dc16e504a96747a1bc134aafb5ef4a15dab7
Iyasele Rehoboth <rehobothi@yahoo.com>
2024-02-21 01:10:22 +0100

    Worked on bottom navigation

--- app/src/main/java/com/iyaselerehoboth/moniepointtakehometest/MainActivity.kt
+++ app/src/main/java/com/iyaselerehoboth/moniepointtakehometest/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.iyaselerehoboth.moniepointtakehometest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.iyaselerehoboth.moniepointtakehometest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        val bottomNav: BottomNavigationView = binding.bottomNavView
+        setupWithNavController(bottomNav, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return super.onSupportNavigateUp() || super.onSupportNavigateUp()
     }
 }
